4aa0973b2729694fe37055ff1f606ef463f11b42
kvitorr <kvitorrsantos@gmail.com>
2023-02-21 23:53:24 -0300

    Adição das rotas de cadastro, login e logout

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,15 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { LoginController } from "./controller/LoginController";
 import { UserController } from "./controller/UserController";
+import { authMiddleware } from "./middlewares/authMiddlewares";
 
 export const routes = Router()
 
 const userController: UserController = new UserController()
 const loginController: LoginController = new LoginController()
 
-routes.get('/user/:email', userController.getUserByEmail)
-routes.post('/user', userController.addUser)
+routes.get('/user', userController.getUsers)
+routes.post('/cadastro', userController.addUser)
 routes.post('/login', loginController.login)
+routes.post('/logout', authMiddleware, loginController.logout)
+
